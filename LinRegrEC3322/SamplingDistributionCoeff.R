e4b6be580a4954ed5d67621027799002b3540e29
CarstenLange <clange@cpp.edu>
2025-04-23 17:45:04 -0700

    Adjusted slides for Spring Semester 2025. Deleted many files. These are present in the IBM or the AIBook Version

--- LinRegrEC3322/SamplingDistributionCoeff.R
+++ LinRegrEC3322/SamplingDistributionCoeff.R
@@ -11,27 +11,30 @@ Data100Houses=sample_n(DataHouses,100)
 ModelOLSOrg=lm(Price~Sqft, data=Data100Houses)
 print(ModelOLSOrg)
 
-# Drawing 10,000 samples running 10,00 OLS Regressiosn
+# Drawing 10,000 samples running 10,000 OLS Regressiosn
 
-Beta2=c()
+Beta0=c()
+Beta1=c()
 
-set.seed(123)
+set.seed(111)
 
 
 for (i in 1:10000) {
-DataTrain=sample_n(DataHousing,100)
+DataTrain=sample_n(Data100Houses,100,replace = TRUE)
 
 ModelOLS=lm(Price~Sqft, data=DataTrain)
 
-Beta1=append(Beta1,ModelOLS$coefficients[[1]])
-Beta2=append(Beta2,ModelOLS$coefficients[[2]])
+Beta0=append(Beta0,ModelOLS$coefficients[[1]])
+Beta1=append(Beta1,ModelOLS$coefficients[[2]])
 }
 
-print(Beta2)
+
 library(TeachHist) 
 
 
-DataBeta2=tibble(beta2=Beta2)
-TeachHistRelFreq(PlotData = DataBeta2, BinWidth = 0.5)
+DataBeta1=tibble(Beta1=Beta1)
+View(DataBeta1)
+TeachHistDens(PlotData = DataBeta1, BinWidth = 0.5)
 
 confint(ModelOLSOrg)
+TeachHistDens(PlotData = DataBeta1, BinWidth = 0.5, VLine1=179, VLine2=300)
