9bd8d6442d07e60c439833d0cef15780fca1f7f0
CarstenLange <clange@cpp.edu>
2025-07-09 17:06:10 -0700

    Updated R script metric= for tune_rid was missing

--- Scripts/DeepLearnScript2.R
+++ Scripts/DeepLearnScript2.R
@@ -40,19 +40,28 @@ FoldsDiamonds= DataTrain %>%
   vfold_cv(v = 7, strata = Price, breaks = 5)
 print(FoldsDiamonds)
 
-# The control argument "control = control_grid(verbose = TRUE)"
-# enables reporting to the console. It has no other function.
-# You can ommit it. Then it is just not as entertaining.
+
+# Note, the package brulee needs to be installed before you run the 
+# tune_grid() command. library(brulee) is not needed.
+# Afterward, when you run tune_grid() th first time you get
+# an error message that torch (for PYTorch) is not installed
+# All you need to do is to confirm the installation of torch
+# and be patient. It takes a while (less than 10 minutes). Do not interrupt,
+# before torch is fully installed and you see the ppromp "<"
+
 set.seed(888)
 TuneResultsDiamonds=tune_grid(WFModelNN,  resamples=FoldsDiamonds, 
                               grid=ParGridDiamonds, 
                               metrics = metric_set(rsq,mae),
                               control = control_grid(verbose = TRUE)) 
+
+# Above, the control argument "control = control_grid(verbose = TRUE)"
+# enables reporting to the console. It has no other function.
+# You can ommit it. Then it is just not as entertaining.
+
 print(TuneResultsDiamonds)
 
-ontrol = control_grid(verbose = TRUE)
-
-BestParNN=select_best(TuneResultsDiamonds,"mae")
+BestParNN=select_best(TuneResultsDiamonds,metric="mae")
 print(BestParNN)
 
 set.seed(888)
