28c2c1e2c31d77c7f51aa53d2b2e1b8d79efbfb6
CarstenLange <clange@cpp.edu>
2026-02-16 23:32:05 -0800

    Chap 6 Exerc. first complete working version

---
+++ aibookexercises/AAAExerciseStructureTempl.R
@@ -0,0 +1,19 @@
+```{webr}
+#| exercise: exc_step2
+#| exercise.setup: exc_all
+
+::: {.hint exercise="exc_step2"}
+**Here is a hint:**
+
+::: {.solution exercise="exc_step2"}
+**Here is the Solution:**
+
+```{webr}
+#| exercise: exc_step2
+#| solution: true
+
+```{webr}
+#| exercise: exc_step2
+#| check: true
+gradethis::grade_this_code()
+```
